6d3fa0b344592ed52e118907c7bc297095755d8e
Wanderson <wanderson991.wb@gmail.com>
2025-03-16 10:10:56 +0000

    feat entity category changes

--- src/domain/category.entity.ts
+++ src/domain/category.entity.ts
@@ -19,7 +19,7 @@ export class Category {
   is_active: boolean;
   created_at?: Date;
 
-  constructor (props: CategoryConstructorProps) {
+  constructor(props: CategoryConstructorProps) {
     this.category_id = props.category_id;
     this.name = props.name;
     this.description = props.description ?? null;
@@ -27,7 +27,33 @@ export class Category {
     this.created_at = props.created_at ?? new Date();
   }
 
-  static create (props: CategoryConstructorProps): Category {
+  static create(props: CategoryConstructorProps): Category {
     return new Category(props);
+  }
+
+  changeName(name: string): void {
+    this.name = name
+  }
+
+  changeDescription(description: string): void {
+    this.description = description
+  }
+
+  active() {
+    this.is_active = true
+  }
+
+  deactivate() {
+    this.is_active = false
+  }
+
+  toJSON() {
+    return {
+      category_id: this.category_id,
+      name: this.name,
+      description: this.description,
+      is_active: this.is_active,
+      created_at: this.created_at
+    }
   }
 }
